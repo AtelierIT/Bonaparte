f277d075a3e3d9d3b385c2287bee3eb90eabf15d
Adrian Caragea <adriangcaragea@gmail.com>
2013-04-02 15:34:44 +0300

    modified category import with the new structure

--- app/code/local/Bonaparte/ImportExport/Model/Custom/Import/Categories.php
+++ app/code/local/Bonaparte/ImportExport/Model/Custom/Import/Categories.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Stores the business logic for the custom category import
+ *
+ * Class Bonaparte_ImportExport_Model_Custom_Import_Categories
+ */
 class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_ImportExport_Model_Custom_Import_Abstract
 {
     const CATEGORY_CODE_PARENT = 'ProductMainGroup';
@@ -10,24 +15,91 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
      */
     public function _construct()
     {
-        $this->_configurationFilePath = '/var/www/bonaparte/magento/dump_files/xml/Cvl.xml';
+        $this->_configurationFilePath = '/var/www/bonaparte/magento/dump_files/xml/structure.xml';
         $this->_initialize();
 
         $config = $this->getConfig();
-        foreach ($config->getNode('group') as $node) {
-            $attributeId = $node->getAttribute('id');
-            if (in_array($attributeId, array(
-                self::CATEGORY_CODE_PARENT,
-                self::CATEGORY_CODE_CHILD
+        $categoryStructure = array();
+        $this->_extractNode($config, $categoryStructure);
+
+        $this->_data = $categoryStructure;
+    }
+
+    /**
+     * Recursive method to extract unknown levels of categories
+     *
+     * @param mixed (Varien_Simplexml_Config|Varien_Simplexml_Element) $node
+     * @param array $categoryStructure
+     */
+    private function _extractNode($node, &$categoryStructure) {
+        if($node instanceof Varien_Simplexml_Config) {
+            $folder = $node->getNode('Folder');
+        } else {
+            $folder = $node->Folder;
+        }
+
+        if(empty($folder)) {
+            return;
+        }
+
+        foreach ($folder as $node) {
+            $attributeId = $node->getAttribute('groupId');
+            $name = (array)$node->locale;
+            $categoryStructure[$attributeId] = array(
+                'name' => $name['value'][0],
+                'children' => array()
+            );
+            $this->_extractNode($node, $categoryStructure[$attributeId]['children']);
+        }
+    }
+
+    /**
+     * Recursive method to add unknown levels of categories
+     *
+     * @param mixed (integer|Mage_Catalog_Model_Category) $parentId
+     * @param array $children
+     */
+    private function _addCategory($parentId, $children) {
+        if(!($parentId instanceof Mage_Catalog_Model_Category)) {
+            $collection = Mage::getModel('catalog/category')->getCollection();
+            $collection->addAttributeToFilter('old_id', $parentId);
+            $collection->load();
+            $parent = array_pop($collection->getItems());
+        } else {
+            $parent = $parentId;
+        }
+
+        foreach($children as $oldCategoryId => $data) {
+            $category = Mage::getModel('catalog/category');
+            $categoryCollection = $category->getCollection();
+            $categoryCollection->addAttributeToFilter('old_id', $oldCategoryId);
+            $categoryCollection->load();
+            foreach($categoryCollection as $duplicateCategory) {
+                $duplicateCategory->delete();
+            }
+
+            $category->setData(array(
+                'name' => $data['name'],
+                'is_active' => 1,
+                'include_in_menu' => 1,
+                'is_anchor' => 0,
+                'url_key' => '',
+                'description' => '',
+                'old_id' => $oldCategoryId
             ))
-            ) {
-                foreach ($node->children() as $childNode) {
-                    foreach ($childNode->children() as $value) {
-                        $value = (array)$value;
-                        $this->_data[$attributeId][$childNode->getAttribute('id')] = $value['value'];
-                    }
-                }
-                ksort($this->_data[$attributeId]);
+                ->setAttributeSetId($category->getDefaultAttributeSetId())
+                ->setStoreId(0)
+                ->setPath(implode('/', $parent->getPathIds()))
+                ->setParentId($parent->getId())
+                ->save();
+
+            unset($categoryCollection, $collection);
+
+            $category->clearInstance();
+            $parent->clearInstance();
+
+            if(!empty($data['children'])) {
+                $this->_addCategory($oldCategoryId, $data['children']);
             }
         }
     }
@@ -38,95 +110,10 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
     public function start()
     {
         // importing parent categories
-        $parentId = Mage::app()->getDefaultStoreView()->getRootCategoryId();
-        $parent = Mage::getModel('catalog/category')->load($parentId);
-        foreach ($this->_data[self::CATEGORY_CODE_PARENT] as $oldCategoryId => $parentCategory) {
-            $category = Mage::getModel('catalog/category');
-
-            $categoryCollection = $category->getCollection();
-            $categoryCollection->addAttributeToFilter('old_id', $oldCategoryId);
-            $categoryCollection->load();
-            foreach($categoryCollection as $duplicateCategory) {
-                $duplicateCategory->delete();
-            }
-
-            $category->setData(array(
-                'name' => trim($parentCategory[2]),
-                'is_active' => 1,
-                'include_in_menu' => 1,
-                'is_anchor' => 0,
-                'url_key' => '',
-                'description' => '',
-                'old_id' => $oldCategoryId
-            ))
-                ->setAttributeSetId($category->getDefaultAttributeSetId())
-                ->setStoreId(0)
-                ->setPath(implode('/', $parent->getPathIds()))
-                ->setParentId($parentId)
-                ->save();
-
-            unset($categoryCollection);
-            $category->clearInstance();
-        }
-
-        // importing child categories
-        $rawCategoryRelations = json_decode(
-            file_get_contents('/var/www/bonaparte/magento/var/tmp/category_relations.json'),
-            true
+        $parent = Mage::getModel('catalog/category')->load(
+            Mage::app()->getDefaultStoreView()->getRootCategoryId()
         );
-
-        $finalCategoryRelations = $multipleCategoryAssignment = array();
-        foreach($rawCategoryRelations as $parentCategory => $childCategories) {
-            foreach($childCategories as $childCategory) {
-                if(isset($finalCategoryRelations[$childCategory])) {
-                    $multipleCategoryAssignment[$childCategory][] = $parentCategory;
-                    continue;
-                }
-                $finalCategoryRelations[$childCategory] = $parentCategory;
-            }
-        }
-        unset($parentCategory, $childCategories, $childCategory);
-
-        $parentId = Mage::app()->getDefaultStoreView()->getRootCategoryId();
-        foreach ($this->_data[self::CATEGORY_CODE_CHILD] as $oldCategoryId => $childCategory) {
-            $category = Mage::getModel('catalog/category');
-
-            $categoryCollection = $category->getCollection();
-            $categoryCollection->addAttributeToFilter('old_id', $oldCategoryId);
-            $categoryCollection->load();
-            foreach($categoryCollection as $duplicateCategory) {
-                $duplicateCategory->delete();
-            }
-
-            if(isset($finalCategoryRelations[$oldCategoryId])) {
-                $parentId = $finalCategoryRelations[$oldCategoryId];
-                $collection = Mage::getModel('catalog/category')->getCollection();
-                $collection->addAttributeToFilter('old_id', $parentId);
-                $collection->load();
-                $parent = array_pop($collection->getItems());
-            } else {
-                $parent = Mage::getModel('catalog/category')->load($parentId);
-            }
-
-            $category->setData(array(
-                'name' => trim($childCategory[2]),
-                'is_active' => 1,
-                'include_in_menu' => 1,
-                'is_anchor' => 0,
-                'url_key' => '',
-                'description' => '',
-                'old_id' => $oldCategoryId
-            ))
-                ->setAttributeSetId($category->getDefaultAttributeSetId())
-                ->setStoreId(0)
-                ->setPath(implode('/', $parent->getPathIds()))
-                ->setParentId($parentId)
-                ->save();
-
-            unset($collection, $categoryCollection);
-            $parent->clearInstance();
-            $category->clearInstance();
-        }
+        $this->_addCategory($parent, $this->_data);
 
         echo 'DONE';
     }
