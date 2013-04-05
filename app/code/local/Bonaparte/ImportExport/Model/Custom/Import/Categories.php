43b2689a7eabb21f6253035ef89fa36c6ce62008
Adrian Caragea <adriangcaragea@gmail.com>
2013-04-05 17:21:07 +0300

    Fixed root categories to be assigned the category structure according to language

--- app/code/local/Bonaparte/ImportExport/Model/Custom/Import/Categories.php
+++ app/code/local/Bonaparte/ImportExport/Model/Custom/Import/Categories.php
@@ -3,32 +3,43 @@
 /**
  * Stores the business logic for the custom category import
  *
- * Class Bonaparte_ImportExport_Model_Custom_Import_Categories
+ * @category    Bonaparte
+ * @package     Bonaparte_ImportExport
+ * @author     Atelier IT Team <office@atelierit.ro>
  */
 class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_ImportExport_Model_Custom_Import_Abstract
 {
-    private $language_index = 0;
-    private $rootCategoryName = array(
-        0  => "da",
-        1  => "ch",
-        2  => "en",
-        3  => "de",
-        4  => "nl",
-        5  => "sv",
-    );
+    /**
+     * Path at which the category configuration is found
+     *
+     * @var string
+     */
+    const CONFIGURATION_FILE_PATH = '/dump_files/xml/structure.xml';
+
+    /**
+     * Language index that will be used to determine what label value to use
+     *
+     * @var int
+     */
+    private $_languageIndex = 0;
+
+    /**
+     * Languages used to create the root categories for each one
+     *
+     * @var array
+     */
+    private $_languages = array('en', 'da', 'ch', 'de', 'nl', 'sv');
+
     /**
      * Construct import model
      */
-    public function _construct()
+    protected function _construct()
     {
-        $this->_configurationFilePath = Mage::getBaseDir().'/dump_files/xml/structure.xml';
+        $this->_configurationFilePath = Mage::getBaseDir() . self::CONFIGURATION_FILE_PATH;
         $this->_initialize();
 
-        $config = $this->getConfig();
-        $categoryStructure = array();
-        $this->_extractNode($config, $categoryStructure);
-
-        $this->_data = $categoryStructure;
+        $this->_data = array();
+        $this->_extractNode($this->getConfig(), $this->_data);
     }
 
     /**
@@ -37,14 +48,15 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
      * @param mixed (Varien_Simplexml_Config|Varien_Simplexml_Element) $node
      * @param array $categoryStructure
      */
-    private function _extractNode($node, &$categoryStructure) {
-        if($node instanceof Varien_Simplexml_Config) {
+    private function _extractNode($node, &$categoryStructure)
+    {
+        if ($node instanceof Varien_Simplexml_Config) {
             $folder = $node->getNode('Folder');
         } else {
             $folder = $node->Folder;
         }
 
-        if(empty($folder)) {
+        if (empty($folder)) {
             return;
         }
 
@@ -52,9 +64,10 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
             $attributeId = $node->getAttribute('groupId');
             $name = (array)$node->locale;
             $categoryStructure[$attributeId] = array(
-                'name' => $name['value'][$this->language_index],
+                'name' => $name['value'],
                 'children' => array()
             );
+
             $this->_extractNode($node, $categoryStructure[$attributeId]['children']);
         }
     }
@@ -65,8 +78,9 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
      * @param mixed (integer|Mage_Catalog_Model_Category) $parentId
      * @param array $children
      */
-    private function _addCategory($parentId, $children) {
-        if(!($parentId instanceof Mage_Catalog_Model_Category)) {
+    private function _addCategory($parentId, $children)
+    {
+        if (!($parentId instanceof Mage_Catalog_Model_Category)) {
             $collection = Mage::getModel('catalog/category')->getCollection();
             $collection->addAttributeToFilter('old_id', $parentId);
             $collection->load();
@@ -75,17 +89,20 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
             $parent = $parentId;
         }
 
-        foreach($children as $oldCategoryId => $data) {
+        foreach ($children as $oldCategoryId => $data) {
             $category = Mage::getModel('catalog/category');
-            $categoryCollection = $category->getCollection();
-            $categoryCollection->addAttributeToFilter('old_id', $oldCategoryId);
-            $categoryCollection->load();
-            foreach($categoryCollection as $duplicateCategory) {
-                $duplicateCategory->delete();
+            $categoryCollection = $category->getCollection()
+                ->addAttributeToFilter('old_id', $oldCategoryId)
+                ->load();
+
+            $name = $data['name'];
+            if(is_array($name)) {
+                $name = $data['name'][$this->_languageIndex];
             }
 
+            $parent->setPathIds(null);
             $category->setData(array(
-                'name' => $data['name'],
+                'name' => $name,
                 'is_active' => 1,
                 'include_in_menu' => 1,
                 'is_anchor' => 0,
@@ -104,8 +121,32 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
             $category->clearInstance();
             $parent->clearInstance();
 
-            if(!empty($data['children'])) {
+            if (!empty($data['children'])) {
                 $this->_addCategory($oldCategoryId, $data['children']);
+            }
+        }
+    }
+
+    /**
+     * Remove category duplicates
+     *
+     * @param array $children
+     */
+    private function _removeDuplicates($children)
+    {
+        foreach ($children as $externalCategoryId => $data) {
+            $categoryCollection = Mage::getModel('catalog/category')->getCollection()
+                ->addAttributeToFilter('old_id', $externalCategoryId)
+                ->load();
+            
+            foreach ($categoryCollection as $duplicateCategory) {
+                $duplicateCategory->delete();
+                $duplicateCategory->clearInstance();
+            }
+            unset($categoryCollection);
+
+            if (!empty($data['children'])) {
+                $this->_removeDuplicates($data['children']);
             }
         }
     }
@@ -115,46 +156,52 @@ class Bonaparte_ImportExport_Model_Custom_Import_Categories extends Bonaparte_Im
      */
     public function start()
     {
-        // importing parent categories
-      //  $parent = Mage::getModel('catalog/category')->load(
-      //      Mage::app()->getDefaultStoreView()->getRootCategoryId()
-      //  );
-        foreach ($this->rootCategoryName as $currentParentLanguageIndex =>$currentParentName){
+        // before importing remove last imported categories
+        $this->_removeDuplicates($this->_data);
 
-            $this->language_index = $currentParentLanguageIndex;
+        foreach ($this->_languages as $languageIndex => $language) {
+            $name = $language . ' root category';
+
+            // also remove all language root categories
+            $categoryCollection = Mage::getModel('catalog/category')->getCollection()
+                ->addAttributeToFilter('name', $name)
+                ->load();
+
+            foreach ($categoryCollection as $duplicateCategory) {
+                $duplicateCategory->delete();
+                $duplicateCategory->clearInstance();
+            }
+
+            unset($categoryCollection);
+
+            $this->_languageIndex = $languageIndex;
 
             // Create category object
-            $category = Mage::getModel('catalog/category');
-            $category->setStoreId(0); // No store is assigned to this category.
+            $category = Mage::getModel('catalog/category')
+                ->setStoreId(0)
+                ->addData(array(
+                    'name' => $name,
+                    'path' => '1',
+                    'description' => 'Category ' . $language,
+                    'meta_title' => 'Meta Title',
+                    'meta_keywords' => 'Meta Keywords',
+                    'meta_description' => 'Meta Description',
+                    'display_mode' => 'PRODUCTS',
+                    'is_active' => 1,
+                    'is_anchor' => 1
+                ));
 
-            $tempRootCategory['name'] = $currentParentName." root category";
-            $tempRootCategory['path'] = "1"; // this is the catgeory path - 1 for root category.
-            $tempRootCategory['description'] = "Category ".$currentParentName;
-            $tempRootCategory['meta_title'] = "Meta Title";
-            $tempRootCategory['meta_keywords'] = "Meta Keywords";
-            $tempRootCategory['meta_description'] = "Meta Description";
-            $tempRootCategory['display_mode'] = "PRODUCTS";
-            $tempRootCategory['is_active'] = 1;
-            $tempRootCategory['is_anchor'] = 1;
-
-            $category->addData($tempRootCategory);
-//            $category->save();
             try {
                 $category->save();
-                $currentParent = $category->getId();
-            }
-            catch (Exception $e){
+            } catch (Exception $e) {
                 echo $e->getMessage();
+                exit;
             }
-//            $currentParent = $category->getId();
-            $parenting = Mage::getModel('catalog/category')->load(
-                Mage::app()->getDefaultStoreView()->getRootCategoryId()
-            );
 
             $this->_addCategory($category, $this->_data);
-            echo 'DONE '.$currentParentName;
         }
 
+        echo 'DONE ';
     }
 
 }
