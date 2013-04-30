fcdf99f5f6257d6e75d72a69e5cb947beaaa69b9
Adrian Caragea <adriangcaragea@gmail.com>
2013-04-30 13:23:50 +0300

    Added price import file

---
+++ app/code/local/Bonaparte/ImportExport/Model/Custom/Import/Prices.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Stores the business logic for the custom price import
+ *
+ * @category    Bonaparte
+ * @package     Bonaparte_ImportExport
+ * @author      Atelier IT Team <office@atelierit.ro>
+ */
+class Bonaparte_ImportExport_Model_Custom_Import_Prices extends Bonaparte_ImportExport_Model_Custom_Import_Abstract
+{
+    /**
+     * Path at which the category configuration is found
+     *
+     * @var string
+     */
+    const CONFIGURATION_FILE_PATH = '/dump_files/ARTIKLAR.TXT';
+
+    /**
+     * Construct import model
+     */
+    public function _construct()
+    {
+        ini_set('memory_limit', '2048M');
+        $this->_logMessage('Reading configuration files');
+        $this->_configurationFilePath = Mage::getBaseDir() . self::CONFIGURATION_FILE_PATH;
+
+        $fileHandler = fopen($this->_configurationFilePath, 'r');
+        $prices = array();
+        $dataKeys = array(
+            'articleno',
+            'headarticle',
+            'countrycode',
+            'articleno2',
+            'headarticle2',
+            'description',
+            'size',
+            'price',
+            'stockdisp',
+            'status',
+            'length',
+            'width',
+            'height',
+            'weight',
+            'showcode',
+            'timestamp',
+            'metrecode',
+            'waitlist',
+            'reg_price',
+            'traffic',
+            'light',
+            'available_date',
+            'preview'
+        );
+
+        $row = 0;
+        $headArticle = null;
+        $temporaryData = array();
+        while($line = fgets($fileHandler)) {
+            $this->_logMessage($row++);
+            $this->_logMemoryUsage();
+            $line = explode(';', $line);
+            $priceData = array();
+            foreach($dataKeys as $key => $value) {
+                $priceData[$value] = $line[$key];
+            }
+            unset($key, $value);
+
+            if($headArticle != $priceData['headarticle']) {
+                if(count($temporaryData[$headArticle])) {
+                    $lowestPrice = null;
+                    foreach($temporaryData[$headArticle] as $data) {
+                        if(($data['price'] < $lowestPrice) || $lowestPrice === null) {
+                            $lowestPrice = $data['price'];
+                        }
+                    }
+                    $this->_data[$headArticle] = $lowestPrice;
+                    unset($data, $lowestPrice);
+                }
+
+                $headArticle = $priceData['headarticle'];
+                unset($temporaryData[$headArticle]);
+            }
+
+            $temporaryData[$headArticle][] = $priceData;
+            unset($priceData);
+            if($row == '50000') {
+                break;
+            }
+        }
+        fclose($fileHandler);
+    }
+
+    /**
+     * Specific category functionality
+     */
+    public function start()
+    {
+        $this->_logMessage('Started importing prices' . "\n" );
+
+        $this->_logMessage('Finished importing prices' . "\n" );
+    }
+
+}
